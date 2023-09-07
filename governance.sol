a5463055898dacdfa6e8f15ade07564d0f7d8026
Diptibaral1993 <33413243+Diptibaral1993@users.noreply.github.com>
2023-09-07 15:08:01 +0530

    Add files via upload

---
+++ governance.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+interface IERC20 {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address to, uint256 amount) external returns (bool);
+    function addUser(address _addressToWhitelist) external;
+    function verifyUser(address _whitelistedAddress) external view returns(bool);
+    function removeFromWhitelist(address[] calldata toRemoveAddresses)external;
+}
+contract TokenBurningContract {
+    address public admin;
+    uint256 public totalVotes;
+    uint count=1;
+
+    mapping(address =>mapping(uint256=>bool) ) public hasVoted;
+    struct proposals{
+        address tokenaddresses;
+        address walletadrress;
+        uint  amount;
+        uint totalvotes;
+        uint timestamp;
+        bool pass;
+    }
+    mapping (uint256=>proposals)public _proposals;
+    constructor() {
+        admin = msg.sender;
+       }
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only admin can call this function");
+        _;
+    }
+    function proposal(address wallet, uint256 _amount,address _tokenaddress) external onlyAdmin {
+            _proposals[count]=proposals(_tokenaddress,wallet,_amount,0,block.timestamp,false);
+            count++;
+    }
+    function vote(uint proposalid) external  {
+        require(_proposals[proposalid].pass==false,"Proposal Already Passed");
+        require(_proposals[proposalid].timestamp!=0,"No created proposalid");
+        require(hasVoted[msg.sender][proposalid]==false, "You have already voted");
+        require(IERC20(_proposals[proposalid].tokenaddresses).verifyUser(msg.sender), "Not whitelisted");
+       
+        _proposals[proposalid].totalvotes+=IERC20(_proposals[proposalid].tokenaddresses).balanceOf(msg.sender);
+
+        if (_proposals[proposalid].totalvotes >=(IERC20(_proposals[proposalid].tokenaddresses).totalSupply()*51)/100) {
+            _proposals[proposalid].pass=true;
+       }
+    }
+    function proposalStatus(uint proposalid) external view returns(bool,address,uint){
+        return  (_proposals[proposalid].pass, _proposals[proposalid].walletadrress, _proposals[proposalid].amount);
+    }
+}
---
+++ vault.sol
@@ -0,0 +1,220 @@
+// SPDX-License-Identifier: GPL-3.0
+// File: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Context.sol
+// OpenZeppelin Contracts v4.4.1 (utils/Context.sol)
+
+pragma solidity 0.8.19;
+
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
+abstract contract Ownable is Context {
+    address private _contractOwner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    constructor() {
+        _transferOwnership(_msgSender());
+    }
+
+    function contractOwner() public view virtual returns (address) {
+        return _contractOwner;
+    }
+
+    function renounceOwnership() public virtual onlyOwner {
+        emit OwnershipTransferred(_contractOwner, address(0));
+        _setContractOwner(address(0));
+    }
+
+    modifier onlyOwner() {
+        require(contractOwner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "New owner cannot be the zero address");
+        emit OwnershipTransferred(_contractOwner, newOwner);
+        _setContractOwner(newOwner);
+    }
+
+    function _transferOwnership(address newOwner) internal {
+        emit OwnershipTransferred(_contractOwner, newOwner);
+        _setContractOwner(newOwner);
+    }
+
+    function _setContractOwner(address newOwner) internal {
+        _contractOwner = newOwner;
+    }
+}
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `to`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address to, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `from` to `to` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
+}
+
+interface IERC20OwnToken {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address to, uint256 amount) external returns (bool);
+    function addUser(address _addressToWhitelist) external;
+    function verifyUser(address _whitelistedAddress) external view returns(bool);
+    function removeFromWhitelist(address[] calldata toRemoveAddresses)external;
+}
+
+contract vault is Ownable
+{
+
+    struct strReward { 
+        address nativeToken;
+        uint256 amount;
+        uint256 lastDepositTime;
+        
+    }
+
+    mapping(address=>strReward) public _Reward;
+
+    struct _usrReward { 
+        uint256 amount;
+        uint256 withdrawn;
+        uint256 lastRewardBalance;
+        uint256 lastWithdrawTime;
+        
+    }
+
+    mapping(address=>mapping(address=>_usrReward)) public usrReward;
+
+    constructor(){
+
+    }
+
+    function depositReward(address _rewardTokenAddress,address _ERC20tokenaddress,uint256 _amount) external virtual onlyOwner returns(bool){
+        
+        uint256 newrewardamt=_Reward[_ERC20tokenaddress].amount + _amount;
+        
+        _Reward[_ERC20tokenaddress]=strReward(_rewardTokenAddress,newrewardamt,block.timestamp);
+        
+        IERC20(_rewardTokenAddress).transferFrom(msg.sender,address(this),_amount);
+        
+        return true;
+    }
+
+    function withdrawReward(address _ERC20tokenaddress) public returns (bool){
+        require(IERC20OwnToken(_ERC20tokenaddress).verifyUser(msg.sender),"Not Whitelist User");
+        require(usrReward[_ERC20tokenaddress][msg.sender].amount!=0,"You dont have any reward to withdraw");
+        
+        usrReward[_ERC20tokenaddress][msg.sender].amount=0;
+        usrReward[_ERC20tokenaddress][msg.sender].lastWithdrawTime=block.timestamp;
+        usrReward[_ERC20tokenaddress][msg.sender].withdrawn+=usrReward[_ERC20tokenaddress][msg.sender].amount;
+        IERC20(_Reward[_ERC20tokenaddress].nativeToken).transfer(msg.sender,usrReward[_ERC20tokenaddress][msg.sender].amount);
+        return true;
+    }
+
+    function setReward(address _wallet,uint256 _amount,uint256 _totalsupply) external returns (bool){
+        require(_wallet!=address(0),"Invalid Wallet");
+        uint256 lastdeposittime=_Reward[msg.sender].lastDepositTime;
+        if(lastdeposittime!=0)
+        {
+            uint256 lastWithdrawtime=usrReward[msg.sender][_wallet].lastWithdrawTime;
+            if(lastdeposittime<lastWithdrawtime || lastWithdrawtime==0)
+            {
+                usrReward[msg.sender][_wallet].amount+=((_Reward[msg.sender].amount-usrReward[msg.sender][_wallet].lastRewardBalance) *(_amount*100)/_totalsupply)/100;
+                usrReward[msg.sender][_wallet].lastRewardBalance=_Reward[msg.sender].amount;
+            }
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+
+
+    function withdrawRewardAdmin(address _token) external virtual onlyOwner returns (bool){
+        IERC20(_Reward[_token].nativeToken).transfer(msg.sender,address(this).balance);
+        return true;
+    }
+
+
+}
+
